f3ec261ad8d15f4eb09d61461c1eca52187f9c87
Mickocar <milovan.maric@gmail.com>
2020-11-01 16:23:48 -0500

    Milovan is trying add some code

--- src/test/java/TC_Milovan.java
+++ src/test/java/TC_Milovan.java
@@ -2,5 +2,9 @@ public class TC_Milovan {
     public static void main(String[] args) {
         System.out.println("Good to have you all working on the same project");
         System.out.println("And one more");
+
+        System.out.println("New try to merge something");
+        System.out.println("Hello everyone");
+
     }
 }
